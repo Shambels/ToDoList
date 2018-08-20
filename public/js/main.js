e0600cccdbdcad2596ff0056a389552e0af309d5
Shambels <MrChnip@gmail.com>
2018-08-20 17:02:16 +0200

    debut JS

---
+++ public/js/main.js
@@ -0,0 +1,33 @@
+var addBtn = document.getElementById("addBtn");
+var toggleToDoBtn = document.getElementById("toggleToDoBtn");
+var toggleDoneBtn = document.getElementById("toggleDoneBtn");
+var toggleDeletedBtn = document.getElementById("toggleDeletedBtn");
+var ul = document.getElementById("ul");
+var form = document.getElementById("form")
+
+
+function toggleToDo() {
+   
+}
+
+function toggleDone() {}
+
+function toggleDeleted() {}
+
+function addLi() {
+   // let li = document.createElement("li");
+   // ul.appendChild(li);
+   // li.createElement("input");
+   // li.createElement("button");
+   // li.createElement("button");
+   // li.createElement("button");
+   ul.write("<li")
+
+}
+
+toggleToDoBtn.addEventListener('click', () => {
+   event.preventDefault();
+   toggleToDo();
+})
+
+addBtn.addEventListener
