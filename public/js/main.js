cc63701f404f5c9f615a05366626d345b865ad57
Shambels <MrChnip@gmail.com>
2018-08-26 22:33:32 +0200

    3.4 -- Toggle All Imp + Flex (semi)Responsive Li's + ===)

--- public/js/main.js
+++ public/js/main.js
@@ -5,6 +5,9 @@ var toggleDoneBtn = document.getElementById("toggleDoneBtn");
 var toggleDeletedBtn = document.getElementById("toggleDeletedBtn");
 var ul = document.getElementById("ul");
 var inputs = Array.from(document.getElementsByTagName("input"));
+let toDoPrevious = "on";
+let donePrevious = "off";
+let deletedPrevious = "off";
 var regSp = /^\s*$/;
 var regSp2 = /^\s*/;
 var regUpperCase = /[a-z]{1}/;
@@ -16,11 +19,13 @@ function addLi(input) {
       if (regUpperCase.test(input.value)) {
          input.value = input.value.replace(input.value.match(regUpperCase)[0], input.value.match(regUpperCase)[0].toUpperCase());
       }
-      ul.innerHTML += '<li class="' + input.etat + ' li row shown"><input type="text" value="' + input.value + '" class="col-9 noEdit mx-2 d-inline form-control" aria-describedby="helpId" readonly><div class="col-2 mx-auto d-inline"><button class="btn btn-light mx-1"><i class="mx-1 fas fa-edit" ></i></button><button class="btn btn-success mx-1"><i class="mx-2 fas fa-check-square"></i></button><button class="btn btn-secondary mx-1"><i class="mx-2 fas fa-trash-alt"></i></button></div></li>';
+      ul.innerHTML += '<li class="' + input.etat + ' li row shown"><input type="text" value="' + input.value + '" class="col-9 noEdit mx-2 d-inline form-control" aria-describedby="helpId" readonly><div class="col-2 mx-auto d-flex"><button class="btn btn-light mx-1"><i class="mx-1 fas fa-edit" ></i></button><button class="btn btn-success mx-1"><i class="mx-2 fas fa-check-square"></i></button><button class="btn btn-secondary mx-1"><i class="mx-2 fas fa-trash-alt"></i></button></div></li>';
+
       addToDoneBtns = Array.from(document.getElementsByClassName("btn-success"));
       addToDeletedBtns = Array.from(document.getElementsByClassName("btn-secondary"));
       editBtns = Array.from(document.getElementsByClassName("btn-light"));
       lis = Array.from(document.getElementsByTagName("li"));
+
       lis.forEach(element => {
          if (!input.etat) {
             element.classList.add("to-do");
@@ -49,13 +54,13 @@ function addLi(input) {
 
 function edit(elem) {
    let c = elem.lastElementChild.children;
-   if (elem.firstElementChild.readOnly == true) {
+   if (elem.firstElementChild.readOnly === true) {
       elem.firstElementChild.addEventListener("keydown", () => checkKey(elem));
       elem.firstElementChild.readOnly = false;
       elem.firstElementChild.focus();
       elem.firstElementChild.select();
       elem.firstElementChild.classList.remove("noEdit");
-      //toggle ( DONE / DEL / + )Buttons
+      //toggle ( DONE / DEL / + ) Buttons +Icons
       for (i = 1; i < c.length; i++) {
          c[i].classList.add("edit");
          c[i].firstElementChild.classList.add("edit");
@@ -89,21 +94,21 @@ function update() {
    doneList = Array.from(document.getElementsByClassName("done"));
    deletedList = Array.from(document.getElementsByClassName("deleted"));
    toDoList.forEach(element => {
-      if (toggleToDoBtn.classList.contains("active") == true) {
+      if (toggleToDoBtn.classList.contains("active") === true) {
          show(element);
       } else {
          hide(element);
       };
    })
    doneList.forEach(element => {
-      if (toggleDoneBtn.classList.contains("active") == true) {
+      if (toggleDoneBtn.classList.contains("active") === true) {
          show(element);
       } else {
          hide(element);
       };
    })
    deletedList.forEach(element => {
-      if (toggleDeletedBtn.classList.contains("active") == true) {
+      if (toggleDeletedBtn.classList.contains("active") === true) {
          show(element);
       } else {
          hide(element);
@@ -112,17 +117,32 @@ function update() {
 }
 
 function toggleAll() {
-   if (toggleToDoBtn.classList.contains("active") == false) {
-      toggleToDo();
-      toggleToDoBtn.classList.add("active");
-   }
-   if (toggleDeletedBtn.classList.contains("active") == false) {
-      toggleDone();
-      toggleDoneBtn.classList.add("active");
-   }
-   if (toggleDeletedBtn.classList.contains("active") == false) {
-      toggleDeleted();
-      toggleDeletedBtn.classList.add("active");
+   if ((toggleToDoBtn.classList.contains("active") === true) && toggleDoneBtn.classList.contains("active") === true && toggleDeletedBtn.classList.contains("active") === true) {
+      if (toDoPrevious === "off") {
+         toggleToDo();
+         toggleToDoBtn.classList.remove("active");
+      }
+      if (donePrevious === "off") {
+         toggleDone();
+         toggleDoneBtn.classList.remove("active");
+      }
+      if (deletedPrevious === "off") {
+         toggleDeleted();
+         toggleDeletedBtn.classList.remove("active");
+      }
+   } else {
+      if (toggleToDoBtn.classList.contains("active") === false) {
+         toggleToDo();
+         toggleToDoBtn.classList.add("active");
+      }
+      if (toggleDoneBtn.classList.contains("active") === false) {
+         toggleDone();
+         toggleDoneBtn.classList.add("active");
+      }
+      if (toggleDeletedBtn.classList.contains("active") === false) {
+         toggleDeleted();
+         toggleDeletedBtn.classList.add("active");
+      }
    }
 }
 
@@ -138,10 +158,18 @@ function toggleToDo() {
       });
    }
 }
+function toggleToDoPrevious() {
+   if (toggleToDoBtn.classList.contains("active") === true) {
+      toDoPrevious = "off";
+   } else {
+      toDoPrevious = "on";
+   }
+}
+
 
 function toggleDone() {
    doneList = Array.from(document.getElementsByClassName("done"));
-   if (toggleDoneBtn.classList.contains("active") == true) {
+   if (toggleDoneBtn.classList.contains("active") === true) {
       doneList.forEach(element => {
          hide(element);
       });
@@ -149,12 +177,20 @@ function toggleDone() {
       doneList.forEach(element => {
          show(element);
       });
+   }
+}
+
+function toggleDonePrevious() {
+   if (toggleDoneBtn.classList.contains("active") === true) {
+      donePrevious = "off";
+   } else {
+      donePrevious = "on";
    }
 }
 
 function toggleDeleted() {
    deletedList = Array.from(document.getElementsByClassName("deleted"));
-   if (toggleDeletedBtn.classList.contains("active") == true) {
+   if (toggleDeletedBtn.classList.contains("active") === true) {
       deletedList.forEach(element => {
          hide(element);
       });
@@ -165,8 +201,16 @@ function toggleDeleted() {
    }
 }
 
+function toggleDeletedPrevious() {
+   if (toggleDeletedBtn.classList.contains("active") === true) {
+      deletedPrevious = "off";
+   } else {
+      deletedPrevious = "on";
+   }
+}
+
 function addToDone(elem) {
-   if (elem.classList.contains("done") == true) {
+   if (elem.classList.contains("done") === true) {
       elem.classList.remove("done");
       elem.classList.add("to-do");
    } else {
@@ -177,8 +221,8 @@ function addToDone(elem) {
 }
 
 function addToDeleted(elem) {
-   if (elem.classList.contains("deleted") == false) {
-      if (elem.classList.contains("to-do") == true && elem.classList.contains("done") == false) {
+   if (elem.classList.contains("deleted") === false) {
+      if (elem.classList.contains("to-do") === true && elem.classList.contains("done") === false) {
          previousClass = "to-do";
          elem.classList.remove("to-do");
          elem.classList.add("deleted");
@@ -205,9 +249,18 @@ function checkKey(input) {
 
 addBtn.addEventListener("click", () => addLi(inputs[0]));
 inputs[0].addEventListener("keydown", () => checkKey(inputs[0]));
-toggleToDoBtn.addEventListener('click', toggleToDo);
-toggleDoneBtn.addEventListener('click', toggleDone);
-toggleDeletedBtn.addEventListener('click', toggleDeleted);
+toggleToDoBtn.addEventListener('click', () => {
+   toggleToDo();
+   toggletoDoPrevious();
+});
+toggleDoneBtn.addEventListener('click', () => {
+   toggleDone();
+   toggleDonePrevious();
+});
+toggleDeletedBtn.addEventListener('click', () => {
+   toggleDeleted();
+   toggleDeletedPrevious()
+});
 toggleAllBtn.addEventListener('click', toggleAll);
 axios.get('base.json').then(function (response) {
    Array.from(response.data).forEach(element => addLi(element));
@@ -216,5 +269,4 @@ axios.get('base.json').then(function (response) {
 // A FAIRE
 //
 //  LOUPE MOUSEOVER : APPRENDRE JQUERY (ex: ZOOMPLE - LEROY ZOOM - BLOWUP.JS - MAGNIFY JS)
-// Dans Json : Check recup = () => this.name;
 // DRAG N DROP
